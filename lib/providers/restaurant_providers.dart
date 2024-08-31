a9f6172917657fef082b2dedb659b745e5e936f9
TanuiBwambok <tandommy12@gmail.com>
2024-08-31 10:48:23 +0300

    Changed provider to read restaurants from the google drive link instead Json file in Assets folder

--- lib/providers/restaurant_providers.dart
+++ lib/providers/restaurant_providers.dart
@@ -1,9 +1,9 @@
+// lib/providers/restaurant_provider.dart
 import 'dart:convert';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/restaurant.dart';
-// import '../models/restaurant.dart';
 
 final restaurantProvider =
     StateNotifierProvider<RestaurantNotifier, List<Restaurant>>((ref) {
@@ -16,11 +16,22 @@ class RestaurantNotifier extends StateNotifier<List<Restaurant>> {
   }
 
   Future<void> _loadRestaurants() async {
-    final String response =
-        await rootBundle.loadString('assets/restaurants.json');
-    final List<dynamic> data = jsonDecode(response);
+    const url =
+        'https://drive.google.com/uc?export=download&id=1uN_gk2oJ5F4JMAsbjThTmER3LffulsZ2';
+    try {
+      final response = await http.get(Uri.parse(url));
 
-    state = data.map((restaurant) => Restaurant.fromJson(restaurant)).toList();
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+        state =
+            data.map((restaurant) => Restaurant.fromJson(restaurant)).toList();
+      } else {
+        throw Exception('Failed to load restaurants');
+      }
+    } catch (e) {
+      print(e);
+      throw Exception('Failed to load restaurants');
+    }
   }
 
   void filterRestaurants(String query) {
